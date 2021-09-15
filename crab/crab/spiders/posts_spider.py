b89fe3418874cdefb1cb9eca039710e610207189
Cooyiming <1184797554@qq.com>
2021-09-15 11:07:07 +0800

    0.001a

---
+++ crab/crab/spiders/posts_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+# define our spider
+class PostsSpider(scrapy.Spider):
+    # 爬虫的唯一标识符
+    name = "posts"
+
+    # 请求生成器
+    def start_requests(self):
+        
+        pn =121
+        url = "https://tieba.baidu.com/p/"+"5389935515"+"?pn="+str(pn)
+
+    def parse(self, response):
+        
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
