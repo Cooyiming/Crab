a3160827457b5abbb4fbc5301777056343e3e8e0
Cooyiming <52604402+Cooyiming@users.noreply.github.com>
2021-09-24 18:21:23 +0800

    1

--- crab/items.py
+++ crab/items.py
@@ -9,14 +9,19 @@ import scrapy
 
 class CrabItem(scrapy.Item):
     # define the fields for your item here like:
-    level = scrapy.Field()
+    metadata = scrapy.Field()
+    post_content = scrapy.Field()
+    comment_num = scrapy.Field()
+    reply_content = scrapy.Field()
+class PostItem(CrabItem):
     uid = scrapy.Field()
-    nickname = scrapy.Field()
+    pid = scrapy.Field()
+    username = scrapy.Field()
+    level = post_no =scrapy.Field()
     content = scrapy.Field()
-    image = scrapy.Field()
-    replycount = scrapy.Field()
-
-
-
-
     pass
+
+class ReplyItem(CrabItem):
+    sid = scrapy.Field()
+    username = scrapy.Field()
+    content = scrapy.Field()
